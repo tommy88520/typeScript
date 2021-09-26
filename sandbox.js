853dc57d5c9f6f6bfcac4079da7e4450a1ca5ae2
tommy88520 <tommy8852024@gmail.com>
2021-09-26 16:21:14 +0800

    示範不能更改類別

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var character = 'jake';
+var age = 20;
+character = 'tommy';
+console.log(character);
+var circle = function (diameter) {
+    return diameter * 20;
+};
+console.log(circle(20));
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+let character = 'jake';
+let age = 20;
+
+character = 'tommy'
+
+console.log(character)
+
+const circle = (diameter:number)=>{
+    return diameter * 20;
+}
+console.log(circle(20))
